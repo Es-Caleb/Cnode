e755856f22c5b8cffc9dcfd97d612dae6cdfb604
LeoPoeX <kingpi@163.com>
2019-04-08 20:43:32 +0800

    unload

--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import iView from 'iview';
 import "./assets/scss/style.scss";
 import "./assets/scss/media.scss";
 import store from "./store";
 import Sidebar from "./components/sidebar/Sidebar";
 import List from "./components/topics-list/TopicsList";
-
+Vue.use(iView);
 
 Vue.config.productionTip = false;
 
 Vue.component("Sidebar", Sidebar);
 Vue.component("List", List);
+
+Vue.filter('fromNow', date => {
+	return moment(date).fromNow();
+});
 
 new Vue({
   router,
