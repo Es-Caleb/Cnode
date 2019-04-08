ef15c778b216d6edf8d9aaf7cbfd58f7df9f8cf0
LeoPoeX <kingpi@163.com>
2019-04-08 10:46:41 +0800

    unload

---
+++ src/components/msg/Msg.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="msg-list">
+        <div class="msg-loading" v-show="loading"></div>
+        <div class="no-msg" v-show="readMessages.length == 0">暂无消息</div>
+        <ul>
+            <li v-for="(item, index) of readMessages" :key="item.id">
+                <div>
+                    <router-link></router-link>
+                    <span></span>
+                    <router-link></router-link>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Msg',
+
+}
+</script>
+
+
+<style lang="scss">
+@import "../../assets/scss/_variable.scss";
+
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Sidebar from './components/sidebar/Sidebar'
+import List from './components/topics-list/TopicsList'
 
 Vue.config.productionTip = false;
+
+Vue.component('Sidebar', Sidebar)
+Vue.component('List', List)
 
 new Vue({
   router,
