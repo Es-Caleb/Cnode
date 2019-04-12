b5884ebd785738ed103df3f58fe79690e98b7917
unknown <1294260255@qq.com>
2019-04-12 16:56:29 +0800

    uploading

--- src/router.js
+++ src/router.js
@@ -6,39 +6,39 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
-      name: 'HomePage',           //主页
-      path: '/',
-      component: () => import('@/views/index/Index'),
+      name: "HomePage", //主页
+      path: "/",
+      component: () => import("@/views/index/Index")
     },
     {
-      name: 'Login',              //登录页
-      path: '/login',
-      component: () => import('@/views/login/Login'),
+      name: "Login", //登录页
+      path: "/login",
+      component: () => import("@/views/login/Login")
     },
     {
-      name: 'User',               //个人主页
-      path: '/user/:loginname',
-      component: () => import('@/views/user/User'),
+      name: "User", //个人主页
+      path: "/user/:loginname",
+      component: () => import("@/views/user/User")
     },
     {
-      name: 'Collections',        //收藏页
-      path: '/user/:loginname/collections',
-      component: () => import('@/views/collections/Collections'),
+      name: "Collections", //收藏页
+      path: "/user/:loginname/collections",
+      component: () => import("@/views/collections/Collections")
     },
     {
-      name: 'Messages',           //未读消息
-      path: '/messages',
-      component: () => import('@/views/messages/Messages'),
+      name: "Messages", //未读消息
+      path: "/messages",
+      component: () => import("@/views/messages/Messages")
     },
     {
-      name: 'Release',                   //发布话题
-      path: '/release/:id',
-      component: () => import('@/views/release/Release'),
+      name: "Release", //发布话题
+      path: "/release/:id",
+      component: () => import("@/views/release/Release")
     },
     {
-      name: 'NotMatch',           //404
-			path: '*',
-			component: () => import('@/views/404/404'),
+      name: "NotMatch", //404
+      path: "*",
+      component: () => import("@/views/404/404")
     }
   ]
 });
