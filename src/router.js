9e64600ebf1351f505738653bc06cc55a872781a
unknown <1294260255@qq.com>
2019-04-12 16:43:36 +0800

    uploading

--- src/router.js
+++ src/router.js
@@ -6,32 +6,37 @@ Vue.use(Router);
 export default new Router({
   routes: [
     {
-      name: 'HomePage',
+      name: 'HomePage',           //主页
       path: '/',
       component: () => import('@/views/index/Index'),
     },
     {
-      name: 'Login',
+      name: 'Login',              //登录页
       path: '/login',
       component: () => import('@/views/login/Login'),
     },
     {
-      name: 'User',
+      name: 'User',               //个人主页
       path: '/user/:loginname',
       component: () => import('@/views/user/User'),
     },
     {
-      name: 'Collections',
+      name: 'Collections',        //收藏页
       path: '/user/:loginname/collections',
       component: () => import('@/views/collections/Collections'),
     },
     {
-      name: 'Messages',
+      name: 'Messages',           //未读消息
       path: '/messages',
       component: () => import('@/views/messages/Messages'),
     },
     {
-      name: 'NotMatch',
+      name: 'Release',                   //发布话题
+      path: '/release/:id',
+      component: () => import('@/views/release/Release'),
+    },
+    {
+      name: 'NotMatch',           //404
 			path: '*',
 			component: () => import('@/views/404/404'),
     }
